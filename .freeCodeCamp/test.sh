00a2c3838879f89cebaa1d2b702606d2166c802f
kleanti bashalli <kleanthisbashalli@gmail.com>
2024-07-15 05:58:53 +0000

    Save progress: 230.1

--- .freeCodeCamp/test.sh
+++ .freeCodeCamp/test.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 QUESTION1="What's your name?"
+QUESTION2="Where are you from?"
 echo $QUESTION1
 read NAME
+echo Hello $NAME.
 
-echo $NAME
+echo $QUESTION2
--- questionnaire.sh
+++ questionnaire.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 QUESTION1="What's your name?"
+QUESTION2="Where are you from?"
 echo $QUESTION1
 read NAME
 echo Hello $NAME.
