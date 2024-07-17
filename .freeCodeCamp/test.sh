f4893c6f683eba584ad63a5f84867afd3afadc52
kleanti bashalli <kleanthisbashalli@gmail.com>
2024-07-17 14:13:23 +0000

    Save progress: 1140.1

--- .freeCodeCamp/test.sh
+++ .freeCodeCamp/test.sh
@@ -3,6 +3,8 @@
 # Program to tell a persons fortune
 
 echo -e "\n~~ Fortune Teller ~~\n"
+N=$(( RANDOM % 6 ))
 RESPONSES=("Yes" "No" "Maybe" "Outlook good" "Don't count on it" "Ask again later")
+echo ${RESPONSES[5]}
 
-declare -p RESPONSES
+echo $N
