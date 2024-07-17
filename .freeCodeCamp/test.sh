9a27998ccc06ebd8db63429157df554c940780ac
kleanti bashalli <kleanthisbashalli@gmail.com>
2024-07-17 05:56:08 +0000

    Save progress: 730.1

--- .freeCodeCamp/test.sh
+++ .freeCodeCamp/test.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
-QUESTION1="What's your name?"
-QUESTION2="Where are you from?"
-QUESTION3="What's your favorite coding website?"
-echo -e "\n~~ Questionnaire ~~\n"
-echo $QUESTION1
-read NAME
-echo Hello $NAME.
-echo $QUESTION2
-read LOCATION
-echo Hello $NAME from $LOCATION.
-echo $QUESTION3
-read WEBSITE
 
-echo $WEBSITE
+# Program that counts down to zero from a given argument
+
+echo -e "\n~~ Countdown Timer ~~\n"
+
+if [[ $1 -gt 0 ]]
+then
+  : '
+  for (( i = $1; i >= 0; i-- ))
+  do
+    echo $i
+    sleep 1
+  done
+  '
+ I=$1
+else
+  echo Include a positive integer as the first argument.
+fi
+
+echo $I
--- countdown.sh
+++ countdown.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
 # Program that counts down to zero from a given argument
-echo -e  "\n~~ Countdown Timer ~~\n"
+
+echo -e "\n~~ Countdown Timer ~~\n"
+
 if [[ $1 -gt 0 ]]
 then
-: '
-for((i = $1; i >= 0; i--))
-do
-	echo $i
-	sleep 1
-done
-'
+  : '
+  for (( i = $1; i >= 0; i-- ))
+  do
+    echo $i
+    sleep 1
+  done
+  '
+ I=$1
 else
   echo Include a positive integer as the first argument.
 fi
